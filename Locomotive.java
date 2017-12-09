bec6088fbb4cf425cfa58363ade6ae9f2547a653
KorbenKross <31703739+KorbenKross@users.noreply.github.com>
2017-12-10 01:03:51 +0400

    Update Locomotive.java

--- Locomotive.java
+++ Locomotive.java
@@ -9,8 +9,6 @@ public abstract class Locomotive implements ITransport {
 
     protected float startPosX;
     protected float startPosY;
-//    protected int countPassengers;
-//////////////////////
     protected int countPassengers;
 
     public int MaxCountPassengers;
@@ -20,7 +18,6 @@ public abstract class Locomotive implements ITransport {
     public int MaxSpeed;
 
     public javafx.scene.paint.Color ColorBody;
-//////////////////////////
 
     public double Weight;
     private Color color;
@@ -28,21 +25,18 @@ public abstract class Locomotive implements ITransport {
     private int speed;
     private int generator;
     private double weight;
-//    public virtual int MaxCountPassengers { protected set; get; }
     public void setMaxCountPassengers(int pas){
         this.pas = pas;
     }
     public int getMaxCountPassengers(){
         return pas;
     }
-//    public virtual int MaxSpeed { protected set; get; }
     public void setMaxSpeed(int speed){
         this.speed = speed;
     }
     public int getMaxSpeed(){
         return speed;
     }
-//    public virtual int MaxCapacityGenerator { protected set; get; }
     public void setMaxCapacityGenerator(int generator){
         this.generator = generator;
     }
@@ -57,9 +51,6 @@ public abstract class Locomotive implements ITransport {
     public Color getColorBody(){
         return color;
     }
-//    public Color ColorBody { protected set; get; }
-
-//    public double Weight { protected set; get; }
     public void setWeight(double weight){
         this.weight = weight;
     }
@@ -68,8 +59,6 @@ public abstract class Locomotive implements ITransport {
     }
 
     public abstract void moveCar();
-
-//    public abstract void setWeight(double weight);
 
     public abstract void drawCar();
 
