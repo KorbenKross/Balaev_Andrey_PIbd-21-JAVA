868bf3a009a8ad1a8a9da38f271392bf83da804b
KorbenKross <31703739+KorbenKross@users.noreply.github.com>
2017-12-10 01:03:04 +0400

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -14,8 +14,6 @@ import javafx.scene.paint.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
-
-//import java.awt.*;
 import java.net.URL;
 
 import java.util.ResourceBundle;
